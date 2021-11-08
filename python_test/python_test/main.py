eefdf292425b2bdd8ada57ebcbc1baab86d28066
myzonexu <well_david@126.com>
2021-11-08 15:36:48 +0800

    UI界面初版

--- python_test/python_test/main.py
+++ python_test/python_test/main.py
@@ -1,1 +1,42 @@
+from PyQt5.Qt import *
+import sys
+from PyQt5.QtWebEngineWidgets import *
+
+# ui_main是.ui文件生成的.py文件名
+from ui_main import Ui_mainWindow
+ 
+class Window(QMainWindow, Ui_mainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+ 
+    def slot1(self):
+        print('QQQQQ')
+ 
+    def func_list(self):
+        self.func()
+ 
+    def func(self):
+        print("hello")
+        #self.widget_monitor=QWebEngineView()
+        #self.widget_monitor.load(QUrl("https://www.baidu.com/"))
+        
+
+        #self.browser=QWebEngineView()
+        #self.browser.load(QUrl("http://www.baidu.com/"))
+
+        #web = QWebEngineView(self.widget_monitor)
+        #web.load(QUrl("http://www.baidu.com/"))
+        #web.show()
+        pass
+ 
+ 
+ 
+ 
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = Window()
+    window.func_list()
+    window.show()
+    sys.exit(app.exec_())
 
---
+++ python_test/python_test/test_main.py
@@ -0,0 +1,44 @@
+
+'''
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtWebEngineWidgets import *
+from robctrl import *
+
+
+class MyWindow(QMainWindow, Ui_MainWindow):
+    def __init__(self, parent=None):
+        super(MyWindow, self).__init__(parent)
+        self.setupUi(self)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    myWin = MyWindow()
+    myWin.show()
+    sys.exit(app.exec_())
+'''
+
+import sys
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtWebEngineWidgets import *
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.setWindowTitle('百度')  #窗口标题
+        self.setGeometry(5,30,1355,730)  #窗口的大小和位置设置
+        self.browser=QWebEngineView()
+        #加载外部的web界面
+        self.browser.load(QUrl('http://www.baidu.com/'))
+        self.setCentralWidget(self.browser)
+if __name__ == '__main__':
+    
+    app=QApplication(sys.argv)
+    win=MainWindow()
+    win.show()
+    app.exit(app.exec_())
